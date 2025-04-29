73e55f68238fb146ca690c1cc9620234fead370d
Partho Sarothi <171351655+parthosarothi46@users.noreply.github.com>
2025-04-30 03:40:02 +0600

    first commit

---
+++ Module 1/first.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+
+    int num1 = 10;
+    num1 = 20;
+    float f = 4.56;
+    char c = '&';
+    printf("Hello World\n");
+    printf("%d\n",num1);
+    printf("%.4f\n",f);
+    printf("%c\n",c);
+    
+    return 0;
+}
---
+++ Module 2/arithmetic.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a = 10;
+    int b = 5;
+    int sum = a + b;
+    int sub = a - b;
+    int mul = a * b;
+    int div = a / b;
+    printf("summation = %d\n",sum);
+    printf("subtraction = %d\n",sub);
+    printf("multiplication = %d\n",mul);
+    printf("division = %d\n",div);
+    return 0;
+}
