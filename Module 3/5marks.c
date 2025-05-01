4e775d59437ea3605856d4ec552226e0f8006295
Partho Sarothi <171351655+parthosarothi46@users.noreply.github.com>
2025-05-01 23:53:57 +0600

    module 3 practice

---
+++ Module 3/5marks.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    int hand_writing_marks = 5;
+    printf("%d",N+hand_writing_marks);
+    return 0;
+}
---
+++ Module 3/8time_table_to_200.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    // for (int i = 1; i <= 200; i++)
+    // {
+    //     printf("%d x %d = %d\n", 8,i,8*i);
+    // }
+
+    int i = 1;
+    while (i<=200)
+    {
+        printf("%d x %d = %d\n", 8,i,8*i);
+        i++;
+    }
+    return 0;
+}
